148abc3a77ab330803f056b0bee20100cdfb2742
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-29 20:16:04 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 ./configure --prefix=${PREFIX} --with-libxc --with-libxc-prefix=${PREFIX} --with-libxc-include=${PREFIX}/include \
             ARCH="x86_64" \
             FOX_LIB="-L${PREFIX}/lib -lFoX_dom -lFoX_sax -lFoX_wxml -lFoX_common -lFoX_utils -lFoX_fsys " \
-            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${SRC_DIR}/S3DE/iotk/include/" \
+            IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${PREFIX}/include -I${SRC_DIR}/S3DE/iotk/include/" \
             SCALAPACK_LIBS="-L${PREFIX}/lib -lscalapack" \
             LAPACK_LIBS="-L${PREFIX}/lib -llapack" \
             BLAS_LIBS="-L${PREFIX}/lib -lblas" \
