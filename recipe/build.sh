53f51d0fddf980ec188e0c8edc55ed79ad6c7b73
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-29 19:54:46 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./configure --prefix=${PREFIX} --with-libxc --with-libxc-prefix=${PREFIX} \
+./configure --prefix=${PREFIX} --with-libxc --with-libxc-prefix=${PREFIX} --with-libxc-include=${PREFIX}/include \
             ARCH="x86_64" \
             FOX_LIB="-L${PREFIX}/lib -lFoX_dom -lFoX_sax -lFoX_wxml -lFoX_common -lFoX_utils -lFoX_fsys " \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${SRC_DIR}/S3DE/iotk/include/" \
