c3c036af3648069e9042036edb889cb356b88387
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-10-25 10:49:10 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,15 @@
             ARCH="x86_64" \
             FOX_LIB="-L${PREFIX}/lib -lFoX_dom -lFoX_sax -lFoX_wxml -lFoX_common -lFoX_utils -lFoX_fsys " \
             IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${SRC_DIR}/S3DE/iotk/include/" \
+            SCALAPACK_LIBS="-L${PREFIX}/lib -lscalapack" \
+            LAPACK_LIBS="-L${PREFIX}/lib -llapack" \
+            BLAS_LIBS="-L${PREFIX}/lib -lblas" \
             CC="${CC}" \
             CPP="${CPP}" \
             LD="mpif90 -fopenmpi -fopenmp" \
-            CFLAGS="${CFLAGS} -L${PREFIX}/lib -lfftw3 -lblas -llapack -lscalapack" \
-            FFLAGS="${FFLAGS} -L${PREFIX}/lib -lfftw3 -lblas -llapack -lscalapack" \
-            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -lfftw3 -lblas -llapack -lscalapack" 
+            CFLAGS="${CFLAGS} -L${PREFIX}/lib -lfftw3" \
+            FFLAGS="${FFLAGS} -L${PREFIX}/lib -lfftw3" \
+            CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -lfftw3" 
  make pwall
  make upf
  make ld1
