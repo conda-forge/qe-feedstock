ce45af341c0e942e38d577d9585159c43b291454
Leopold Talirz <leopold.talirz@gmail.com>
2021-07-21 02:29:22 +0200

    add more executables

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -ex
 
-#export ARCH="x86_64"
-#export FOX_LIB="-L${PREFIX}/lib -lFoX_dom -lFoX_sax -lFoX_wxml -lFoX_common -lFoX_utils -lFoX_fsys "
-#export IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude -I${SRC_DIR}/S3DE/iotk/include/"
-#export BLAS_LIBS="-L${PREFIX}/lib -lblas"
-#export SCALAPACK_LIBS="-L${PREFIX}/lib -lscalapack"
-#export LAPACK_LIBS="-L${PREFIX}/lib -llapack"
-#export FFT_LIBS="-L${PREFIX}/lib -lfftw3"
-#export MANUAL_DFLAGS="-D__FFTW3"
-#
 ## Override C and Fortran preprocessor
 #export CPP="${CC} -E -P"
 #export FPP="${FC} -E -P -cpp"
@@ -20,29 +11,6 @@ set -ex
 # (will be fixed in next release)
 ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-cpp $BUILD_PREFIX/cpp
 
-#
-##export CC="${CC}"
-#export CFLAGS="${CFLAGS} -fopenmp"
-##export FC="${FC}"
-#export FFLAGS="${FFLAGS} -fopenmp"
-#export LD="mpif90"
-#export LDFLAGS="${LDFLAGS} -fopenmp"
-#
-#./configure \
-#    --prefix=${PREFIX} \
-#    --enable-parallel \
-#    --enable-openmp \
-#    --with-scalapack
-
-## download fox
-#rm -rf external/fox
-##git clone https://github.com/pietrodelugas/fox/tree/98ce8e36c881ccf511c1c4991ff76c174eaaeab9 external/fox
-#wget https://github.com/pietrodelugas/fox/archive/98ce8e36c881ccf511c1c4991ff76c174eaaeab9.tar.gz
-#tar -xf 98ce8e36c881ccf511c1c4991ff76c174eaaeab9.tar.gz -C external/
-#mv external/fox-98ce8e36c881ccf511c1c4991ff76c174eaaeab9 external/fox
-
-
-
 mkdir build
 cd build
 cmake .. \
@@ -52,14 +20,7 @@ cmake .. \
     -DQE_ENABLE_TEST=ON \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}
     
-make
-
-
-
-#make pwall
-#make ld1
-#make cp
-#make epw
+make pwall ld1 cp epw
 
 ## see https://gitlab.com/QEF/q-e/-/blob/develop/test-suite/Makefile
 #cd test-suite
