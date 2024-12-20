a8c4d7db315d147b6ae7f16d9161115eb5e59fe0
H. L. Nourse <32994022+thenoursehorse@users.noreply.github.com>
2024-12-20 11:43:25 +1100

    fix: correctly pass cmake args setup by conda-forge internally

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ cd build
 
 # FOR MPIEXEC configuration, see https://cmake.org/cmake/help/v3.9/module/FindMPI.html
 
-cmake .. \
+cmake ${CMAKE_ARGS} .. \
     -DQE_ENABLE_MPI=ON \
     -DQE_ENABLE_OPENMP=ON \
     -DQE_ENABLE_SCALAPACK=ON \
